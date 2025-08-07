b01515d92a238dcbbb2e69f5154af45dd0d6ccb8
Angelo <apsanchez9@espe.edu.ec>
2025-08-07 08:36:48 -0500

    cambio en eslit.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 export default [
     {
         files: ['**/*.js'],
-        languajeOptions: {
+        languageOptions: {
             ecmaVersion: 'latest',
             sourceType: 'module',  
             
